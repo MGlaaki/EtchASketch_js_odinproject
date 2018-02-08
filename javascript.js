dccb14f2703757be42a2d7199fe43e1bf423fc83
Glaaki <glaaki@protonmail.com>
2018-02-08 19:20:59 +0100

    Minor corrections

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
+buildBoard(50);
+
 function changeColor(e){
   var color = (document.getElementById("radioBlack").checked)? "black" : "random" ;
-  console.log(document.getElementById("radioBlack").checked);
-  console.log(document.getElementById("radioRandom").checked);
 
   if (color == "black") document.getElementById(e.target["id"]).style.backgroundColor = "black";
   else document.getElementById(e.target["id"]).style.backgroundColor = randomColor();
@@ -13,17 +13,12 @@ function buildBoard(n){
     container.removeChild(container.firstChild);
   }
 
-
-  while (n%(Math.sqrt(n)) != 0) n++;
-
-
-
-  for (i=0 ; i<n ; i++){
+  for (i=0 ; i<n*n ; i++){
     var div = document.createElement("div");
     div.setAttribute("id", "div"+(i+1));
     div.style.backgroundColor = "white";
-    div.style.border = "thin solid black";
-    div.style.gridColumn = i%(Math.sqrt(n)) +1;
+    div.style.border = "thin solid grey";
+    div.style.gridColumn = i%n +1;
     document.getElementById("container").appendChild(div);
     document.getElementById("div" + (i+1)).addEventListener("mouseover" , function(e){changeColor(e);});
   }
@@ -42,10 +37,10 @@ function reset(){
 function setupBoard(){
   var n = "z" ;
   while (isNaN(n) || n === "" ){
-    n = prompt("Combien de cases ? (max1024)");
+    n = prompt("Combien de cases de côté ? (max 50)");
   }
   if (n < 0) n = -n ;
-  if (n>1024) n=1024 ;
+  if (n>50) n=50 ;
   if (n != null) buildBoard(n);
 }
 
